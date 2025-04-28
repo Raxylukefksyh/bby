53aa0fbfcd827c9a1b6cd6b89885a837855c9842
Raxylukefksyh <samaelgg4@gmail.com>
2025-04-28 11:37:53 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+# Placeholder for future database models if needed
+# Currently no database models are required for this Discord bot
+
+class BotConfig:
+    """
+    Configuration class for storing bot-related settings
+    Can be expanded in future to include more configuration options
+    """
+    AUTHORIZED_ROLE_ID = 1366100715033858190
+    CUSTOMER_ROLE_ID = 1366095881912193026
